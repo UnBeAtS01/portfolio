eb1fb193a2890136eaf231621203e7010e9d4023
unbeat01 <unbeatablesinha01@gmail.com>
2021-03-28 20:54:03 +0530

    changes

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Navbar from '../navbar/navbar';
 import './about.scss';
-import { Route } from 'react-router-dom';
+import { Route, Router } from 'react-router-dom';
 import AboutPage from '../../pages/about/about';
 import ProjectPage from '../../pages/project/project';
+import SomethingPage from '../../pages/something/something';
 class About extends React.Component {
     state = {
         present: 'About'
@@ -18,7 +19,7 @@ class About extends React.Component {
 
                 <Route exact path='/' component={AboutPage} />
                 <Route exact path='/projects' component={ProjectPage} />
-
+                <Route exact path='/something' component={SomethingPage} />
 
 
             </div>
---
+++ src/pages/something/something.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './something.scss';
+class Something extends React.Component {
+    render() {
+        return (
+            <div className='something'>Nothing here Still</div>
+        )
+    }
+}
+export default Something;
