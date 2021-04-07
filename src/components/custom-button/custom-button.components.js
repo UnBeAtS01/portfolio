a71be379335cc260b24663a864d6612b3f5c2941
unbeat01 <unbeatablesinha01@gmail.com>
2021-04-07 16:47:54 +0530

    changes

--- src/components/custom-button/custom-button.components.js
+++ src/components/custom-button/custom-button.components.js
@@ -4,7 +4,7 @@ import './custom-button.styles.scss';
 
 const CustomButton = ({ children, resume, ...otherProp }) => {
     return (
-        <button className={` custom-button ${resume ? 'resume' : ''}`} {...otherProp}>
+        <button className={`  ${resume ? 'resume' : 'custom-button'}`} {...otherProp}>
             { children}
         </button >
     )
